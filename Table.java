4066fb01a224a91f973e3facac1e3046ef0a2767
nish97 <sanjiv.nishanth@gmail.com>
2017-04-02 11:41:45 +0530

    Add files via upload

--- Table.java
+++ Table.java
@@ -1,19 +1,25 @@
 import java.util.*;
 class Table
 {
-public static void main(String args[])
-{
-int n,x=0,i,y;
-Scanner s=new Scanner(System.in);
-System.out.println("enter the table number:");
-n=s.nextInt();
-Scanner t=new Scanner(System.in);
-System.out.println("enter the range:");
-y=t.nextInt();
-for(i=1;i<=y;i++)
-{
-x=n*i;
-System.out.println(n+ "X" +i+"=" +x);
-}
+  public static void main(String args[])
+  {
+   int i=1,a,n=10;
+   String ch;
+   Scanner s = new Scanner(System.in);
+   System.out.println("Enter the Table number:"); 
+   a = s.nextInt();
+   do
+    {  
+       for(;;i++)
+        {
+          System.out.println(i+"x"+a+"="+(i*a));
+          if(i==n)
+          break;    
+        }
+       System.out.println("Do you want to extend table(yes/no):");
+       ch = s.next();
+       n+=10;
+       i+=1;    
+    }while(ch.equals("yes"));
 }
 }
